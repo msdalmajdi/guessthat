046d52c44182c61402abbfb1e8b5533cacf55b3c
Msdalmajdi <msdalmajdi@hotmail.com>
2022-05-11 20:18:33 +0300

    FINISHED The random guess

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import Intro from "./components/Intro";
 import Entry from "./components/Entry";
 import Helps from "./components/Helps";
+
 function App() {
   return (
     <div className="container">
       <Intro />
+      <div className="wrong"></div>
       <Entry />
       <Helps />
-      <div className="keepers">things to ease the stuff</div>
+      <div className="keepers">{} Attempts left</div>
     </div>
   );
 }
--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,12 +1,27 @@
+const secretNumber = Math.floor(Math.random() * 10000);
+
+const guess = (theGuess) => {
+  if (theGuess === secretNumber) {
+    alert(`Correct`);
+  } else {
+    alert(`${theGuess} is not equal to ${secretNumber}`);
+  }
+};
 const Entry = () => {
   return (
     <div className="entry">
       <input
+        id="guess"
         type="number"
         className="guess-field"
         placeholder="0000-9999"
       ></input>
-      <button className="unlock-button">Unlock</button>
+      <button
+        className="unlock-button"
+        onClick={() => guess(document.getElementById("guess").value)}
+      >
+        Unlock
+      </button>
     </div>
   );
 };
